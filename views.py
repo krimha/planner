ff0700912cc52f1d06b24c9ce29a2e4c4c7d7ce1
Kristian Monsen Haug <kristian.haug95@gmail.com>
2018-04-08 16:22:03 +0200

    List all tasks

--- views.py
+++ views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import Task
 
 def index(request):
-    return render(request, 'planner/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'planner/index.html', {'tasks': tasks})
+
+
