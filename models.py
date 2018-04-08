dc60f559b926bd5169a53ce1c28f41163246f036
Kristian Monsen Haug <kristian.haug95@gmail.com>
2018-04-08 16:13:45 +0200

    Create basic Task model

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
