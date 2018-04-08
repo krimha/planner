623699d189532d248ad38843841c49bd1b0196ba
Kristian Monsen Haug <kristian.haug95@gmail.com>
2018-04-08 19:52:23 +0200

    Toggling of status

--- views.py
+++ views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 
 from .models import Task
 
 def index(request):
     tasks = Task.objects.all()
     return render(request, 'planner/index.html', {'tasks': tasks})
+
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.done ^= True
+    task.save()
+    return redirect('index')
 
 
