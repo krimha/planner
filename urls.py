b6586757e80119d5a10e21c1e331d1f032c692c3
Kristian Monsen Haug <kristian.haug95@gmail.com>
2018-04-16 20:41:48 +0200

    Organize templating

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('tasks', views.tasks, name='tasks'),
     path('complete/<int:task_id>', views.toggle_task, name='toggle'),
     path('add', views.add_task, name='add'),
 ]
