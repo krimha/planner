8bc7055f8eaa31843e7990fcb2808ce8ccd5c151
Kristian Monsen Haug <kristian.haug95@gmail.com>
2018-04-16 20:23:31 +0200

    Revert "Merge branch 'ajax' into development"
    This reverts commit 908636826085eaa3d8795265f3ef2719aa62f120, reversing
    changes made to 08b859783cd28e24786d0f4113d6295c1b1dd83d.

--- views.py
+++ views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.decorators.http import require_POST
-from django.http import HttpResponse
 
 from .models import Task
 from .forms import TaskForm
@@ -15,27 +14,21 @@ def index(request):
     }
     return render(request, 'planner/index.html', context)
 
-@require_POST
-def toggle_task(request):
-    # TODO: Should this be done using a form, and more safely
-    if request.is_ajax():
-        task_id = request.POST['task_id']
-        task = get_object_or_404(Task, pk=task_id)
-        task.done ^= True
-        task.save()
 
-    return render(request, 'planner/task.html', {'task': task})
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    task.done ^= True
+    task.save()
+    return redirect('index')
 
 @require_POST
 def add_task(request):
 
     form = TaskForm(request.POST)
 
-    if form.is_valid() and request.is_ajax():
+    if form.is_valid():
         task = Task(title=request.POST['task_title'])
         task.save()
 
-        return render(request, 'planner/task.html', {'task': task})
-
-    return HttpResponse(status_code=400)
+    return redirect('index')
 
